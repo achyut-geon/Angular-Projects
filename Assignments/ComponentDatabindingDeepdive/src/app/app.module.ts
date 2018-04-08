0f267832a7c55f65d3b0f007a1d9de3564a53749
Achyut Menon <achyut.menon@gadgeon.com>
2018-04-08 17:00:08 +0530

    Intermediate Commit

--- Assignments/ComponentDatabindingDeepdive/src/app/app.module.ts
+++ Assignments/ComponentDatabindingDeepdive/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    EvenComponent,
+    OddComponent
   ],
   imports: [
     BrowserModule
